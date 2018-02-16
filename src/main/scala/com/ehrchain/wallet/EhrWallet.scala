648cab85a255b3c8c1bea3ba3b7e4ce0bc18ddf3
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2018-02-16 12:43:55 +0200

    Adds empty wallet;

---
+++ src/main/scala/com/ehrchain/wallet/EhrWallet.scala
@@ -0,0 +1,24 @@
+package com.ehrchain.wallet
+
+import com.ehrchain.block.EhrBlock
+import com.ehrchain.transaction.EhrTransaction
+import scorex.core.VersionTag
+import scorex.core.transaction.box.proposition.PublicKey25519Proposition
+import scorex.core.transaction.wallet.Vault
+import scorex.core.utils.ScorexLogging
+
+import scala.util.Try
+
+case class EhrWallet() extends Vault[PublicKey25519Proposition, EhrTransaction, EhrBlock, EhrWallet]
+  with ScorexLogging {
+
+  override type NVCT = this.type
+
+  override def scanOffchain(tx: EhrTransaction): EhrWallet = this
+
+  override def scanOffchain(txs: Seq[EhrTransaction]): EhrWallet = this
+
+  override def scanPersistent(modifier: EhrBlock): EhrWallet = this
+
+  override def rollback(to: VersionTag): Try[EhrWallet] = Try { this }
+}
