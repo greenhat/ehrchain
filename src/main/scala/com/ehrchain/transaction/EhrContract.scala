cc3172396b18fa8bf4d6ce668ce1d3d3f8b483ff
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2018-02-21 17:24:18 +0200

    Moves term and timestamp to EhrContract trait;

--- src/main/scala/com/ehrchain/transaction/EhrContract.scala
+++ src/main/scala/com/ehrchain/transaction/EhrContract.scala
@@ -4,14 +4,22 @@ import java.time.Instant
 
 import scorex.core.transaction.box.proposition.PublicKey25519Proposition
 
-trait EhrContract {
-
-  def validity: Either[Throwable, Boolean]
-}
-
 trait EhrContractTerm {}
 case object Unlimited extends EhrContractTerm
 final case class ValidUntil(date: Instant) extends EhrContractTerm
+
+trait EhrContract {
+
+  val timestamp: Instant
+  val term: EhrContractTerm
+
+  def validity: Either[Throwable, Boolean] = term match {
+    case Unlimited => Right[Throwable, Boolean](true)
+    case ValidUntil(date) if timestamp.compareTo(date) < 0 => Right[Throwable, Boolean](true)
+    case ValidUntil(date) if timestamp.compareTo(date) >= 0 =>
+      Left[Throwable, Boolean](new Exception("contract ValidUntil date is less than contract's timestamp"))
+  }
+}
 
 final case class EhrAppendContract(patientPK: PublicKey25519Proposition,
                                    providerPK: PublicKey25519Proposition,
@@ -19,10 +27,4 @@ final case class EhrAppendContract(patientPK: PublicKey25519Proposition,
                                    term: EhrContractTerm)
   extends EhrContract {
 
-  override def validity: Either[Throwable, Boolean] = term match {
-    case Unlimited => Right[Throwable, Boolean](true)
-    case ValidUntil(date) if timestamp.compareTo(date) < 0 => Right[Throwable, Boolean](true)
-    case ValidUntil(date) if timestamp.compareTo(date) >= 0 =>
-      Left[Throwable, Boolean](new Exception("contract ValidUntil date is less than contract's timestamp"))
-  }
 }
