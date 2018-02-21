95e2b62ecceabf3fb54b87f9b42673a44b2547cb
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2018-02-21 16:13:11 +0200

    Removes encryption step from the append contract; Adds generic transaction validity description;

--- src/main/scala/com/ehrchain/transaction/EhrContract.scala
+++ src/main/scala/com/ehrchain/transaction/EhrContract.scala
@@ -5,7 +5,7 @@ import scorex.core.transaction.box.proposition.PublicKey25519Proposition
 trait EhrContract {
 }
 
-final case class EhrAppendContract(encryptedPatientPK: Array[Byte],
+final case class EhrAppendContract(patientPK: PublicKey25519Proposition,
                              providerPK: PublicKey25519Proposition)
   extends EhrContract {
 }
