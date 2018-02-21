805d743b8f5aefb6370bcd1cfcec521b940a304d
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2018-02-21 17:22:05 +0200

    Adds EhrContractTerm; Adds EhrAppendContract.term;

--- src/main/scala/com/ehrchain/transaction/EhrContract.scala
+++ src/main/scala/com/ehrchain/transaction/EhrContract.scala
@@ -1,11 +1,28 @@
 package com.ehrchain.transaction
 
+import java.time.Instant
+
 import scorex.core.transaction.box.proposition.PublicKey25519Proposition
 
 trait EhrContract {
+
+  def validity: Either[Throwable, Boolean]
 }
 
+trait EhrContractTerm {}
+case object Unlimited extends EhrContractTerm
+final case class ValidUntil(date: Instant) extends EhrContractTerm
+
 final case class EhrAppendContract(patientPK: PublicKey25519Proposition,
-                                   providerPK: PublicKey25519Proposition)
+                                   providerPK: PublicKey25519Proposition,
+                                   timestamp: Instant,
+                                   term: EhrContractTerm)
   extends EhrContract {
+
+  override def validity: Either[Throwable, Boolean] = term match {
+    case Unlimited => Right[Throwable, Boolean](true)
+    case ValidUntil(date) if timestamp.compareTo(date) < 0 => Right[Throwable, Boolean](true)
+    case ValidUntil(date) if timestamp.compareTo(date) >= 0 =>
+      Left[Throwable, Boolean](new Exception("contract ValidUntil date is less than contract's timestamp"))
+  }
 }
