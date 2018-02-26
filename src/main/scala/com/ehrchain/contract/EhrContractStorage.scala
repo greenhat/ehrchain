196fd6e2926d7986b4f11ca530b02c4854e32513
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2018-02-26 11:31:23 +0200

    Implements EhrInMemoryContractStorage;

--- src/main/scala/com/ehrchain/contract/EhrContractStorage.scala
+++ src/main/scala/com/ehrchain/contract/EhrContractStorage.scala
@@ -2,15 +2,24 @@ package com.ehrchain.contract
 
 import scorex.core.transaction.box.proposition.PublicKey25519Proposition
 
+import scala.util.{Failure, Success, Try}
+
 trait EhrContractStorage {
 
-  def add(contract: EhrContract): Unit
+  def add(contract: EhrContract): Try[Unit]
   def contractsForPatient(patientPK: PublicKey25519Proposition): Seq[EhrContract]
 }
 
+@SuppressWarnings(Array("org.wartremover.warts.MutableDataStructures", "org.wartremover.warts.Var"))
 class EhrInMemoryContractStorage extends EhrContractStorage {
 
-  override def add(contract: EhrContract): Unit = ???
+  private val store: scala.collection.mutable.Map[String, EhrContract] = scala.collection.mutable.Map()
 
-  override def contractsForPatient(patientPK: PublicKey25519Proposition): Seq[EhrContract] = ???
+  override def add(contract: EhrContract): Try[Unit] = contract match {
+    case append: EhrAppendContract => Success(store(append.patientPK.address) = append)
+    case _ => Failure[Unit](new Error("unknown contract type"))
+  }
+
+  override def contractsForPatient(patientPK: PublicKey25519Proposition): Seq[EhrContract] =
+    store.get(patientPK.address).map(Seq(_)).getOrElse(Seq[EhrContract]())
 }
---
+++ src/test/scala/com/ehrchain/contract/EhrInMemoryContractStorageSpec.scala
@@ -0,0 +1,30 @@
+package com.ehrchain.contract
+
+import com.ehrchain.EhrGenerators
+import org.scalatest.{Matchers, PropSpec, Succeeded}
+import org.scalatest.prop.{GeneratorDrivenPropertyChecks, PropertyChecks}
+
+import scala.util.Success
+
+class EhrInMemoryContractStorageSpec extends PropSpec
+  with PropertyChecks
+  with GeneratorDrivenPropertyChecks
+  with Matchers
+  with EhrGenerators {
+
+  property("add") {
+    val storage = new EhrInMemoryContractStorage()
+    forAll(ehrAppendContractUnlimitedGen) { b: EhrAppendContract =>
+      storage.add(b) shouldBe Success()
+    }
+  }
+
+  property("retrieve") {
+    val storage = new EhrInMemoryContractStorage()
+    forAll(ehrAppendContractUnlimitedGen) { b: EhrAppendContract =>
+      storage.add(b).map( _ =>
+        storage.contractsForPatient(b.patientPK) shouldEqual Seq(b)
+      ) shouldBe Success(Succeeded)
+    }
+  }
+}
