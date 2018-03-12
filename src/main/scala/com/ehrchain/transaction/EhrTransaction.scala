3ac7b4c448b16a2934bf560f05d3be18700a88b1
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2018-03-12 08:58:24 +0200

    comment;

--- src/main/scala/com/ehrchain/transaction/EhrTransaction.scala
+++ src/main/scala/com/ehrchain/transaction/EhrTransaction.scala
@@ -6,14 +6,6 @@ import scorex.core.transaction.Transaction
 import scorex.core.transaction.box.proposition.PublicKey25519Proposition
 import scorex.core.transaction.proof.Signature25519
 
-
-//sealed trait EhrTransactionKind
-//case object AppendContract extends EhrTransactionKind
-//case object AppendRecord extends EhrTransactionKind
-//case object ReadContract extends EhrTransactionKind
-//case object RevokeAppendContract extends EhrTransactionKind
-
-
 @SuppressWarnings(Array("org.wartremover.warts.LeakingSealed"))
 trait EhrTransaction extends Transaction[PublicKey25519Proposition] with BytesSerializable {
 
