17d181cb52ad2336d3d1d77634c41cb26ff35855
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2018-03-29 11:40:57 +0300

    Adds a test for no missing files in a record transaction;

--- src/test/scala/ehr/record/RecordFileDownloaderSupervisorSpec.scala
+++ src/test/scala/ehr/record/RecordFileDownloaderSupervisorSpec.scala
@@ -3,11 +3,15 @@ package ehr.record
 import akka.actor.ActorSystem
 import akka.testkit.TestProbe
 import akka.testkit.typed.scaladsl.{BehaviorTestKit, Effects}
+import ehr.EhrGenerators
 import ehr.record.RecordFileDownloaderSupervisor.DownloadMissingFiles
 import org.scalatest.{FlatSpec, Matchers}
 
-@SuppressWarnings(Array("org.wartremover.warts.Nothing", "org.wartremover.warts.NonUnitStatements"))
-class RecordFileDownloaderSupervisorSpec extends FlatSpec with Matchers {
+@SuppressWarnings(Array("org.wartremover.warts.Nothing", "org.wartremover.warts.NonUnitStatements",
+  "org.wartremover.warts.OptionPartial"))
+class RecordFileDownloaderSupervisorSpec extends FlatSpec
+  with Matchers
+  with EhrGenerators {
 
 implicit lazy val system: ActorSystem = ActorSystem()
 
@@ -20,6 +24,20 @@ implicit lazy val system: ActorSystem = ActorSystem()
         peerManager.ref)
     )
     testKit.run(DownloadMissingFiles(Seq()))
+    testKit.retrieveEffect() shouldEqual Effects.NoEffects
+    testKit.isAlive shouldBe false
+  }
+
+  it should "don't spawn a downloader and terminate if no missing files" in {
+    val peerManager = TestProbe()
+    val fileStorage = InMemoryRecordFileStorageMock.storage
+    val testKit = BehaviorTestKit(
+      RecordFileDownloaderSupervisor.behavior(
+        fileStorage,
+        peerManager.ref)
+    )
+    val tx = ehrRecordTransactionGen.sample.get
+    testKit.run(DownloadMissingFiles(Seq(tx)))
     testKit.retrieveEffect() shouldEqual Effects.NoEffects
     testKit.isAlive shouldBe false
   }
