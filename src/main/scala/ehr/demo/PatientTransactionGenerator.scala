dd27cfdd9db8d7399c336ad9e8395a676c8efc48
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2018-04-03 16:47:06 +0300

    Creates appends contract in patient role;

--- src/main/scala/ehr/demo/PatientTransactionGenerator.scala
+++ src/main/scala/ehr/demo/PatientTransactionGenerator.scala
@@ -1,15 +1,25 @@
 package ehr.demo
 
+import java.time.Instant.now
+
 import akka.actor.ActorRef
 import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.scaladsl.Behaviors.same
+import ehr.contract.{AppendContract, Unlimited}
+import ehr.crypto.Curve25519KeyPair
 import ehr.demo.TypedActorWrapper.NodeViewHolderCallback
 import ehr.transaction.ContractTransaction
 import scorex.core.LocallyGeneratedModifiersMessages.ReceivableMessages.LocallyGeneratedTransaction
 import scorex.core.transaction.box.proposition.PublicKey25519Proposition
+import scorex.core.transaction.state.PrivateKey25519Companion
 
 object PatientTransactionGenerator {
+
+  val patientKeyPair: Curve25519KeyPair =
+    PrivateKey25519Companion.generateKeys("patient seed".getBytes)
+  val providerAKeyPair: Curve25519KeyPair =
+    PrivateKey25519Companion.generateKeys("provider A seed".getBytes)
 
   def behavior(viewHolderRef: ActorRef): Behavior[NodeViewHolderCallback] =
     Behaviors.immutable[NodeViewHolderCallback] { (ctx, msg) =>
@@ -21,7 +31,10 @@ object PatientTransactionGenerator {
       }
     }
 
-  def appendTx: ContractTransaction = ???
+  def appendTx: ContractTransaction =
+    ContractTransaction.generate(patientKeyPair,
+      AppendContract(patientKeyPair.publicKey, providerAKeyPair.publicKey, now, Unlimited),
+      now)
 
 }
 
