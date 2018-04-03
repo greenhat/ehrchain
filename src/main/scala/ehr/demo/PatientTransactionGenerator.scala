fce105b949fa08156a71eae93054268f2927a8d6
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2018-04-03 18:05:21 +0300

    Adds read contract transaction creation to the patient role;

--- src/main/scala/ehr/demo/PatientTransactionGenerator.scala
+++ src/main/scala/ehr/demo/PatientTransactionGenerator.scala
@@ -6,7 +6,7 @@ import akka.actor.ActorRef
 import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.scaladsl.Behaviors.same
-import ehr.contract.{AppendContract, Unlimited}
+import ehr.contract.{AppendContract, ReadContract, RecordKeys, Unlimited}
 import ehr.crypto.Curve25519KeyPair
 import ehr.demo.TypedActorWrapper.NodeViewHolderCallback
 import ehr.transaction.ContractTransaction
@@ -14,24 +14,38 @@ import scorex.core.LocallyGeneratedModifiersMessages.ReceivableMessages.LocallyG
 import scorex.core.transaction.box.proposition.PublicKey25519Proposition
 import scorex.core.transaction.state.PrivateKey25519Companion
 
+@SuppressWarnings(Array("org.wartremover.warts.TryPartial"))
 object PatientTransactionGenerator {
 
   val patientKeyPair: Curve25519KeyPair =
     PrivateKey25519Companion.generateKeys("patient seed".getBytes)
   val providerAKeyPair: Curve25519KeyPair =
     PrivateKey25519Companion.generateKeys("provider A seed".getBytes)
+  val providerBKeyPair: Curve25519KeyPair =
+    PrivateKey25519Companion.generateKeys("provider B seed".getBytes)
 
   val appendTx: ContractTransaction =
     ContractTransaction.generate(patientKeyPair,
       AppendContract(patientKeyPair.publicKey, providerAKeyPair.publicKey, now, Unlimited),
       now)
 
+  val readTx: ContractTransaction =
+    ContractTransaction.generate(patientKeyPair,
+      ReadContract.generate(patientKeyPair,
+        providerBKeyPair.publicKey,
+        now,
+        RecordKeys.build(patientKeyPair, Seq(providerAKeyPair.publicKey))).get,
+      now
+    )
+
   def behavior(viewHolderRef: ActorRef): Behavior[NodeViewHolderCallback] =
-    Behaviors.immutable[NodeViewHolderCallback] { (ctx, msg) =>
+    Behaviors.immutable[NodeViewHolderCallback] { (_, msg) =>
       msg match {
-        case NodeViewHolderCallback(wallet) =>
+        case NodeViewHolderCallback(_) =>
           viewHolderRef !
             LocallyGeneratedTransaction[PublicKey25519Proposition, ContractTransaction](appendTx)
+          viewHolderRef !
+            LocallyGeneratedTransaction[PublicKey25519Proposition, ContractTransaction](readTx)
           same
       }
     }
